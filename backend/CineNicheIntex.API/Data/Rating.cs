602c4d110de7a64e7f9a82f4c38f301d0b22d0f5
tracewilson <wiltrac3@byu.edu>
2025-04-07 15:05:33 -0600

    help

---
+++ backend/CineNicheIntex.API/Data/Rating.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CineNicheIntex.API.Data
+{
+    public class Rating
+    {
+        [Key]
+        public int user_id { get; set; }
+        public string show_id { get; set; }
+        public int rating { get; set; }
+
+    }
+}
