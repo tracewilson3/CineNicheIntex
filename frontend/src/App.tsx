af4afbddc124072b314a1a283a35cbb221706f84
cxxpertaylor <cooper.b.taylor@gmail.com>
2025-04-07 19:31:56 -0600

    added privacy policy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,17 @@
+import MoviesPage1 from "./pages/MoviesPage1";
 
-import MoviesPage1 from './pages/MoviesPage1';
-
-
-
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import MoviesPage from './pages/MoviesPage';
-import LoginPage from './pages/login';
-import SignupPage from './pages/signup';
-import TempleScene from './pages/templescene';
-import AdminMoviePage from './pages/AdminMoviePage';
-import AdminUsersPage from './pages/AdminUsersPage';
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import MoviesPage from "./pages/MoviesPage";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
+import TempleScene from "./pages/templescene";
+import AdminMoviePage from "./pages/AdminMoviePage";
+import AdminUsersPage from "./pages/AdminUsersPage";
+import PrivacyPage from "./pages/PrivacyPage";
 
 function App() {
   return (
-
-        
-
-    
-
-
-    
-
     <Routes>
       <Route path="/" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
@@ -30,8 +20,8 @@ function App() {
       <Route path="/AdminPage" element={<AdminMoviePage />} />
       <Route path="/movies1" element={<MoviesPage1 />} />
       <Route path="/AdminUsers" element={<AdminUsersPage />} />
+      <Route path="/privacy" element={<PrivacyPage />} />
     </Routes>
-
   );
 }
 
