777d9c97f0f1e8e65c6250f1f1dd2fb6108526c6
tracewilson <wiltrac3@byu.edu>
2025-04-09 11:30:44 -0600

    grant stuff merge

--- backend/CineNicheIntex.API/obj/Debug/net8.0/CineNicheIntex.API.AssemblyInfo.cs
+++ backend/CineNicheIntex.API/obj/Debug/net8.0/CineNicheIntex.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb3b76da19b3e01df6177f1ad792fb4f0c75da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39524fe28657b2198168366a08fe83d331a923c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
