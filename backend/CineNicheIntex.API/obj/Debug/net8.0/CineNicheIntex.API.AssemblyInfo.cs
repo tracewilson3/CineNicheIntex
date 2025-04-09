42f5d926176cafe85ee2fc8db1d142edad03c27a
tracewilson <wiltrac3@byu.edu>
2025-04-09 11:43:54 -0600

    deploymadness

--- backend/CineNicheIntex.API/obj/Debug/net8.0/CineNicheIntex.API.AssemblyInfo.cs
+++ backend/CineNicheIntex.API/obj/Debug/net8.0/CineNicheIntex.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffb3b76da19b3e01df6177f1ad792fb4f0c75da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc542b07fa4ec3ac9100a6926f704dd1643636dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineNicheIntex.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
